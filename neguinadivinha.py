2e7befa6ea4cd46582bf6301f161c65b9e8a76ac
JHONPLIQUIS <126591413+JHONPLIQUIS@users.noreply.github.com>
2025-03-07 08:55:54 -0300

    Add files via upload

--- neguinadivinha.py
+++ neguinadivinha.py
@@ -2,24 +2,33 @@ print ("****************************")
 print ("Bem Vinvo ao Neguin Adivinha")
 print ("****************************")
 
-
-chute = int(input("Digite o numero secreto: "))
-
 numero_secreto = 24 
-acertou = numero_secreto == chute
-chute_maior = chute > numero_secreto
-chute_menor = chute < numero_secreto
+total_de_tentativas = 3 
 
 
-print(f"Você digitou {chute}\n")
+for rodada in range  (1,total_de_tentativas):
+    print(f"tentativas (rodada) de {total_de_tentativas}")
+    chute = int(input("digite o seu numero: "))
 
-if(acertou):
-    print("Você acertou\n")
-else:
-    if(chute > numero_secreto):
-        print("muito burro! Seu numero foi maior que o numero secreto \n")
-    elif(chute < numero_secreto):
-        print("inbecil! seu numero foi menor que o numero secreto")
+
+    numero_secreto = 24 
+    acertou = numero_secreto == chute
+    chute_maior = chute > numero_secreto
+    chute_menor = chute < numero_secreto
+
+
+    print(f"Você digitou {chute}\n")
+
+    if(acertou):
+        print("Você acertou\n")
+        break
+
+    else:
+        if(chute_maior):
+            print("muito burro! Seu numero foi maior que o numero secreto \n")
+        elif(chute_menor):
+            print("inbecil! seu numero foi menor que o numero secreto")
+
 
 
 print ("@@@@@@@@@@@")
